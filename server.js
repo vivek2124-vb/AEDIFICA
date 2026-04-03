790da9b6327d2c4c9a5692bd476db0ecbc40219d
Vivek Bisht <vivekvb2124@gmail.com>
2026-04-04 01:01:05 +0530

    Fix CORS origin URL formatting in server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-     "https://aedifica.in
+     "https://aedifica.in"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
