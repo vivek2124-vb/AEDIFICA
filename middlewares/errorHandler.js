56e5327072fd8b3cb97694e85d6a4042dee6dd48
Vivek Bisht <vivekvb2124@gmail.com>
2026-04-04 20:04:45 +0530

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
+  console.error("❌ Global Error:", err);
+
   res.status(err.statusCode || 500).json({
     success: false,
     message: err.message || "Server Error",
