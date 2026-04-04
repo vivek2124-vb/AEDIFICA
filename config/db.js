dbd43a0e463ef9d1e876f543a6072042f0df18d8
Vivek Bisht <vivekvb2124@gmail.com>
2026-04-04 20:06:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(" DB Error:", error.message);
+    console.error("❌ DB Error:", error.message);
     process.exit(1);
   }
 };
